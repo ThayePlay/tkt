90a775fbbb7e7f3f61032f46d2f8f06b82497e8f
ThayePlay <133688708+ThayePlay@users.noreply.github.com>
2023-06-04 11:39:25 +0200

    Add files via upload

---
+++ M&.lua
@@ -0,0 +1,63 @@
+﻿local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ThayePlay/MTScript/main/MT_Gui_OPL_Def.lua')))()
+local Player = game.Players.LocalPlayer
+local Window = OrienLib:MakeWindow({Name = "Key System", HidenPrenium = false, SaveConfig = true, IntroEnabled = false})
+
+OrionLib:MakeNotification({
+    Name = "Logged In !",
+Content = "You are logged in as "..Player.Name..-.-
+Image == "rbxassetid://4483345998",
+Time = 5
+})
+
+_G.Key = "MTopl987"
+_G.Keyinput = "string"
+
+function MakeScriptHub ()
+    local Window = OrienLib:MakeWindow({Name = "Script Hub", HidenPrenium = false, SaveConfig = true, IntroEnabled = true, IntroText"Script Hub"})
+end
+
+function CorrectKeyNotification()
+    OrionLib:MakeNotification({
+        Name = "Correct Key!",
+    Content = "You have entered the coorect key!",
+    Image == "rbxassetid://4483345998",
+    Time = 5
+    }) 
+end
+
+function IncorrectKeyNotification()
+    OrionLib:MakeNotification({
+        Name = "Incorrect Key!",
+    Content = "You have entered the incorect key!"
+   , Image == "rbxassetid://4483345998",
+    Time = 5
+    }) 
+end
+
+
+local Tab = Window:MakeTab({
+    Name = "Key",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly= false
+})
+
+Tab:AddTextbox({
+    Name = "Enter Key",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+       G.KeyInput = Value
+    end
+})
+
+Tab:AddButton({
+	Name = "Check Key !",
+	Callback = function()
+      		if _G.Keyinput == _G.Key then
+         MakeScriptHub()
+         CorrectKeyNotification()
+            else
+                IncorrectKeyNotification()
+            end       
+  	end    
+})
